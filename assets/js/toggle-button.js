7a008ef0ca26cd567220d540de876d96b5cc3210
Zakaria Alsahfi <zakaria.sahfi@gmail.com>
2023-05-16 11:19:06 +0300

    Update toggle-button.js

--- assets/js/toggle-button.js
+++ assets/js/toggle-button.js
@@ -5,13 +5,13 @@ function toggleButton() {
 
   if (html.classList.contains('dark_mode')) {
     toggleButton.setAttribute('aria-pressed', 'true');
-    toggleButton.innerHTML = '<i class="material-icons toggle-button__icon toggle-button__icon--sun">wb_sunny</i><span 
-    class="toggle-button__text">Light Mode</span><i class="material-icons toggle-button__icon toggle-button__icon--moon">brightness_2</i>';
+    toggleButton.innerHTML = '<i class="material-icons toggle-button__icon toggle-button__icon--sun">wb_sunny</i><i class="material-icons toggle-button__icon toggle-button__icon--moon">brightness_2</i>';
   } else {
     toggleButton.setAttribute('aria-pressed', 'false');
-    toggleButton.innerHTML = '<i class="material-icons toggle-button__icon toggle-button__icon--sun">wb_sunny</i><span 
-    class="toggle-button__text">Dark Mode</span><i class="material-icons toggle-button__icon toggle-button__icon--moon">brightness_2</i>';
+    toggleButton.innerHTML = '<i class="material-icons toggle-button__icon toggle-button__icon--sun">wb_sunny</i><i class="material-icons toggle-button__icon toggle-button__icon--moon">brightness_2</i>';
   }
+
+  toggleButton.classList.add('toggle-button--hidden');
 }
 
 // Add event listener to toggle button
