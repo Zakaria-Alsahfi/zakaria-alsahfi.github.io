c215b6df1a295400a616d9e38aa8a7e1e9c78425
Zakaria Alsahfi <zakaria.sahfi@gmail.com>
2023-05-16 10:54:24 +0300

    Delete dark.js

--- assets/js/dark.js
+++
@@ -1,9 +0,0 @@
-// Toggle dark mode
-function toggleDarkMode() {
-  var html = document.querySelector('html');
-  html.classList.toggle('dark_mode');
-}
-
-// Add event listener to toggle button
-var toggleButton = document.querySelector('#theme-toggle');
-toggleButton.addEventListener('click', toggleDarkMode);
