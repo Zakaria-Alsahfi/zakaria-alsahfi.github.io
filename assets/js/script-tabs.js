3f9ee18399e057b1aab614e8233ea47a896fddf3
Zakaria Alsahfi <zakaria.sahfi@gmail.com>
2023-05-18 16:19:21 +0300

    Update script-tabs.js

--- assets/js/script-tabs.js
+++ assets/js/script-tabs.js
@@ -1,29 +1,13 @@
-// Get all the navigation links
-var navLinks = document.querySelectorAll('.nav-pills li');
+const navLinks = document.querySelectorAll('.nav-pills li');
+const tabContent = document.querySelectorAll('.tab-content');
 
-// Get all the tab content
-var tabContent = document.querySelectorAll('.tab-content');
-
-// Add click event listener to each navigation link
-navLinks.forEach(function(navLink) {
-  navLink.addEventListener('click', function() {
-    event.preventDefault(); // Prevent the default behavior of the link
-    
-    // Remove the active class from all navigation links
-    navLinks.forEach(function(link) {
-      link.parentElement.classList.remove('active');
-    });
-
-    // Add the active class to the clicked navigation link
-    this.parentElement.classList.add('active');
-
-    // Hide all the tab content
-    tabContent.forEach(function(tab) {
-      tab.classList.remove('active');
-    });
-
-    // Show the tab content for the clicked navigation link
-    var tabId = this.getAttribute('data-tab');
+navLinks.forEach(navLink => {
+  navLink.addEventListener('click', event => {
+    event.preventDefault();
+    navLinks.forEach(link => link.parentElement.classList.remove('active'));
+    event.currentTarget.parentElement.classList.add('active');
+    tabContent.forEach(tab => tab.classList.remove('active'));
+    const tabId = event.currentTarget.getAttribute('data-tab');
     document.getElementById(tabId).classList.add('active');
   });
 });
