9dbb5e0d4a0d2f8e5d88c852962588a536afdeaa
Zakaria Alsahfi <zakaria.sahfi@gmail.com>
2023-05-21 08:58:26 +0300

    Update script-tabs.js

--- assets/js/script-tabs.js
+++ assets/js/script-tabs.js
@@ -1,13 +1,20 @@
-const navLinks = document.querySelectorAll('.nav-pills li');
+// Get all the tabs and tab content
+const tabs = document.querySelectorAll('.nav-pills li');
 const tabContent = document.querySelectorAll('.tab-content');
 
-navLinks.forEach(navLink => {
-  navLink.addEventListener('click', event => {
-    event.preventDefault();
-    navLinks.forEach(link => link.parentElement.classList.remove('active'));
-    event.currentTarget.parentElement.classList.add('active');
-    tabContent.forEach(tab => tab.classList.remove('active'));
-    const tabId = event.currentTarget.getAttribute('data-tab');
-    document.getElementById(tabId).classList.add('active');
+// Add click event listeners to all the tabs
+tabs.forEach(tab => {
+  tab.addEventListener('click', e => {
+    e.preventDefault();
+
+    // Remove the active class from all tabs and tab content
+    tabs.forEach(tab => tab.classList.remove('active'));
+    tabContent.forEach(content => content.classList.remove('active'));
+
+    // Add the active class to the clicked tab and corresponding tab content
+    const tabId = e.target.closest('li').getAttribute('data-tab');
+    const activeTab = document.querySelector(`#${tabId}`);
+    e.target.closest('li').classList.add('active');
+    activeTab.classList.add('active');
   });
 });
