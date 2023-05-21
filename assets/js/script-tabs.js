6eab9fe9e09140796c89c73ac825ef34bc960833
Zakaria Alsahfi <zakaria.sahfi@gmail.com>
2023-05-21 15:21:44 +0300

    Update script-tabs.js

--- assets/js/script-tabs.js
+++ assets/js/script-tabs.js
@@ -1,19 +1,52 @@
-const tabs = document.querySelectorAll('.nav-pills li a');
+<!--
+//const tabs = document.querySelectorAll('.nav-pills li a');
+//const tabContents = document.querySelectorAll('.tab-content');
+//tabs.forEach((tab, tabIndex) => {
+  //tab.addEventListener('click', (event) => {
+    //event.preventDefault();
+
+    //tabs.forEach((t) => {
+     // t.classList.remove('active');
+    //});
+
+    //tabContents.forEach((c) => {
+     // c.classList.remove('active');
+    //});
+
+   // tab.classList.add('active');
+   // tabContents[tabIndex].classList.add('active');
+  //});
+//}); 
+-->
+const tabLinks = document.querySelectorAll('.tab-link');
+const redLine = document.querySelector('.red-line');
 const tabContents = document.querySelectorAll('.tab-content');
 
-tabs.forEach((tab, tabIndex) => {
-  tab.addEventListener('click', (event) => {
-    event.preventDefault();
+tabLinks.forEach(link => {
+  link.addEventListener('click', e => {
+    e.preventDefault();
+    const tabId = e.target.getAttribute('data-tab');
+    const currentTab = document.querySelector(`#${tabId}`);
+    const currentLink = e.target;
 
-    tabs.forEach((t) => {
-      t.classList.remove('active');
+    tabContents.forEach(tab => {
+      tab.classList.remove('active');
     });
 
-    tabContents.forEach((c) => {
-      c.classList.remove('active');
+    tabLinks.forEach(link => {
+      link.classList.remove('active');
     });
 
-    tab.classList.add('active');
-    tabContents[tabIndex].classList.add('active');
+    currentTab.classList.add('active');
+    currentLink.classList.add('active');
+
+    const activeLink = document.querySelector('.tab-link.active');
+    const activeLinkWidth = activeLink.offsetWidth;
+    const activeLinkPosition = activeLink.getBoundingClientRect().left;
+    const tabsContainer = document.querySelector('.tabs');
+    const tabsContainerPosition = tabsContainer.getBoundingClientRect().left;
+    const newRedLinePosition = activeLinkPosition - tabsContainerPosition;
+    redLine.style.transform = `translateX(${newRedLinePosition}px)`;
+    redLine.style.width = `${activeLinkWidth}px`;
   });
 });
