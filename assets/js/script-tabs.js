46fbf3a13860bf9188c17cd29d6a809d48c3ebf3
Zakaria Alsahfi <zakaria.sahfi@gmail.com>
2023-05-21 10:10:02 +0300

    Update script-tabs.js

--- assets/js/script-tabs.js
+++ assets/js/script-tabs.js
@@ -1,36 +1,19 @@
-// Get all the tabs and tab content
-const tabs = document.querySelectorAll('.nav-pills li');
-const tabContent = document.querySelectorAll('.tab-content');
+const tabs = document.querySelectorAll('.nav-pills li a');
+const tabContents = document.querySelectorAll('.tab-content');
 
-// Add click event listeners to all the tabs
-tabs.forEach(tab => {
-  tab.addEventListener('click', e => {
-    e.preventDefault();
+tabs.forEach((tab, tabIndex) => {
+  tab.addEventListener('click', (event) => {
+    event.preventDefault();
 
-    // Get the tab ID from the data attribute
-    const tabId = e.target.closest('li').dataset.tab;
-
-    // Remove the active class from all tabs and tab content
-    tabs.forEach(tab => tab.classList.remove('active'));
-    tabContent.forEach(content => {
-      content.classList.remove('active');
-      content.style.opacity = '0';
+    tabs.forEach((t) => {
+      t.classList.remove('active');
     });
 
-    // Add the active class to the clicked tab and corresponding tab content
-    e.target.closest('li').classList.add('active');
-    const activeTab = document.querySelector(`[data-tab="${tabId}"]`);
-    activeTab.classList.add('active');
+    tabContents.forEach((c) => {
+      c.classList.remove('active');
+    });
 
-    // Fade in the active tab content
-    setTimeout(() => {
-      activeTab.style.opacity = '1';
-    }, 50);
-
-    // Fade out the previous tab content
-    const previousTab = document.querySelector('.tab-content.active');
-    setTimeout(() => {
-      previousTab.style.opacity = '0';
-    }, 0);
+    tab.classList.add('active');
+    tabContents[tabIndex].classList.add('active');
   });
 });
