cbd83611f02e0bd2fbbb51934416295ba92788e8
Zakaria Alsahfi <zakaria.sahfi@gmail.com>
2023-05-27 20:48:44 +0300

    Delete script-iframe.js

--- assets/js/script-iframe.js
+++
@@ -1,9 +0,0 @@
-window.onload = function() {
-	var iframe = document.getElementById("my-iframe");
-	if (window.innerWidth <= 768) {
-		iframe.style.height = "300px";
-	}
-	else {
-		iframe.style.height = "400px";
-	}
-};
